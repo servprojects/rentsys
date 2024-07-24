ddf79dacbfec7ed2b3d3cd4db43f9e00939b1268
Jacky Avenido <>
2024-07-24 20:49:43 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,6 @@ Route::get('login', [AuthController::class, 'index'])->name('login');
 Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post'); 
 Route::get('registration', [AuthController::class, 'registration'])->name('register');
 Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post'); 
+Route::get('dashboard', [AuthController::class, 'dashboard']); 
 Route::get('/', [AuthController::class, 'dashboard']); 
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
