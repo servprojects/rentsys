e31cf20266f0f240e1ac7bb12ec2d70a65a1f543
Jacky Avenido <>
2024-07-28 22:51:12 +0800

    save sample item

--- app/Http/Requests/ItemBrandStoreRequest.php
+++ app/Http/Requests/ItemBrandStoreRequest.php
@@ -11,7 +11,7 @@ class ItemBrandStoreRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,16 +1,82 @@
-
 @extends('layouts.layout')
-   
+
 @section('content')
-<div class="card">
-  <div class="card-header">
-    Item Form
-  </div>
-  <div class="card-body">
-    <blockquote class="blockquote mb-0">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-      <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-    </blockquote>
-  </div>
-</div>
+    <div class="p-lg-5">
+        <div class="card w-100 w-lg-50">
+            <form action="{{ route('items.store') }}" method="POST">
+                @csrf
+                <div class="card-header">
+                    ITEM FORM
+                </div>
+                <div class="card-body ">
+                    <div>
+                        <div class="mb-3">
+                            <label for="brand" class="form-label">Generic Name</label>
+                            <select name="item_generic_name_id" class="form-control">
+                                <option selected>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                            @error('item_generic_name_id')
+                                <div class="form-text text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="brand" class="form-label">Brand</label>
+                            <select name="item_brand_id" class="form-control">
+                                <option selected></option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                            @error('item_generic_name_id')
+                                <div class="form-text text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="brand" class="form-label">Category</label>
+                            <select name="item_category_id" class="form-control">
+                                <option selected></option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                            @error('item_category_id')
+                                <div class="form-text text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Description</label>
+                            <input type="text" name="description" class="form-control" id="description">
+                            @error('description')
+                                <div class="form-text text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="model" class="form-label">Model</label>
+                            <input type="text" name="model" class="form-control" id="model">
+                            @error('model')
+                                <div class="form-text text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="details" class="form-label">Other Details</label>
+                            <textarea class="form-control" name="details" id="details" rows="3"></textarea>
+                            @error('details')
+                                <div class="form-text text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                    </div>
+                    <footer>
+                        <div class="d-flex justify-content-end">
+                            <button type="button" class="btn btn-secondary mr-2">Cancel</button>
+                            <button type="submit" class="btn btn-primary">Save</button>
+                        </div>
+                    </footer>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
